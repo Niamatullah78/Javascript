af6ad79c8c8172b3e20dba93e6cb512191ebecf2
Niamatullah AHMADZAI <105276497+Niamatullah78@users.noreply.github.com>
2022-06-03 16:52:25 +0200

    javascript

---
+++ Exercices/js/03_tva.js
@@ -0,0 +1,8 @@
+var horsTaxe = prompt("Prix HT : ");
+
+console.log(horsTaxe * 1.2);
+
+console.log("Le prix TTC est de : " + (horsTaxe * 1.2) + '€.');
+
+document.write("<h3>Résultat<br /> Le prix TTC est de " + (horsTaxe * 1.2) + '€. </h3>');
+
---
+++ Initiation/js/03.js
@@ -0,0 +1,59 @@
+// alert("Nouha");
+
+// -- 1. Addition
+// -- INFO -- Je peux déclarer plusieurs variables à la suite
+
+//  je décalre mes variables 
+
+var nb1, nb2, resultat;
+
+// j'affiche des valeurs 
+nb1 = 10;
+nb2 = 5;
+
+// j'utiilise mes variables avec un calcul 
+resultat = nb1 + nb2;
+
+// j'affiche mon résultat dans la console 
+console.log(resultat);
+
+// -- 2. Soustraction
+// -- Soustraction de nb1 - nb2 avec l'opérateur " - "
+resultat = nb1 - nb2;
+console.log(resultat);
+
+// -- 3. Multiplication
+// -- Multiplication de nb1 * nb2 avec l'opérateur "*"
+resultat = nb1 * nb2;
+console.log(resultat);
+
+// 4. Division
+// -- Division avec l'opérateur  "/"
+resultat = nb1 / nb2;
+console.log(resultat);
+
+// 5. Modulo
+// -- le modulo retourne le reste d'une Division
+// -- modulo de nb1 % nb2 avec l'opérateur "%"
+resultat = nb1 % nb2;
+console.log(resultat);
+
+// -- et maintenant je ré-affecte une valeur à nb1
+nb1 = 11;
+resultat = nb1 % nb2;
+console.log( "Le reste de la division de " + nb1  + " par " + nb2 + " est égal à : " + resultat);
+
+// -- 6. Ecritures simplifiées
+nb1 = 15;
+nb1 = nb1 + 5;// égal à 20
+
+nb1 += 10; // nb1 lui-même + quelque chose
+console.log('Le résultat de "nb1 += 10;" est ' + nb1);// égal à 30, alternance de quillemet simples et doubles pour ne pas casser ma chaine de caractères
+
+
+// le raccourci est le même avec autres opérateurs +, -, /, *, %
+
+// Si j'uilise un ' (apostrophe) dans une phrase concaténée, je peux échapper le caractère à l'aide d'un anti-slash \ (alt gr + 8)
+
+// FIN
+
